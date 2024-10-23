ac3b3a3e3de07c55cedf353b33c82104b8e17ca5
Srilekha hatui <hatuisrilekha@gmail.com>
2024-10-23 15:52:12 +0530

    Update sum_of_enterednum.java

--- assignmentwork/sum_of_enterednum.java
+++ assignmentwork/sum_of_enterednum.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+
+public class Eighth {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		//System.out.println("enter the numbers:");
+		int sum=0;
+		int n;
+		do {
+			System.out.println("enter the numbers:");
+		     n=input.nextInt();
+			 sum+=n;
+		}while(n!=0);
+		System.out.println(sum);
+	}
+
+}
 
